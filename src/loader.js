e9af315d38e80f4cd358651b9ebd356d7697d3fa
vitorsilvalima <vitorsilvalima29@gmail.com>
2019-12-18 23:33:18 -0300

    chore: delete useless files

--- src/loader.js
+++
@@ -1,8 +0,0 @@
-function loadNavigation() {
-  require('../src/screens/App/BankAccounts/EditBankAccount/index.navigation.js'),
-  require('../src/screens/App/BankAccounts/AddBankAccount/AccountInfoForm/index.navigation.js')
-}
-
-export {
-  loadNavigation,
-}
